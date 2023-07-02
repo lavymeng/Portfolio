88122322af1dfa899adfa80e7eea6c94409f0c57
lavymeng <lavymeng@gmail.com>
2023-07-02 14:23:12 +0700

    test

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -3,7 +3,7 @@ import React from "react";
 const Aboutme = () => {
     return (
       <div className="w-2/5 m-auto">
-        <p className="font-semibold">About me</p>
+        <p className="font-semibold">About me Hello</p>
         <p>
           A curious person who is passionate about Front-end web development. I
           am hard working and willing to learn new things and capable of working
