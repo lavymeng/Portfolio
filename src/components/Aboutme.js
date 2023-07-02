9a84d9d674ef7c2be951669c426bf68fa00988e9
lavymeng <lavymeng@gmail.com>
2023-07-02 13:41:18 +0700

    clean file

---
+++ src/components/Aboutme.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Aboutme = () => {
+    return (
+      <div className="w-2/5 m-auto">
+        <p className="font-semibold">About me</p>
+        <p>
+          A curious person who is passionate about Front-end web development. I
+          am hard working and willing to learn new things and capable of working
+          with both individuals and teams.
+        </p>
+      </div>
+    );
+};
+
+export default Aboutme;
